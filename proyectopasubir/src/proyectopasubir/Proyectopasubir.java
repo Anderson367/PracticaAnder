625933f9f771d7c83844a39c9f20dd411b88b45e
Personal <Personal@DESKTOP-PGD11MA>
2020-05-28 20:13:43 -0500

    creando un proyecto nuevo

--- proyectopasubir/src/proyectopasubir/Proyectopasubir.java
+++ proyectopasubir/src/proyectopasubir/Proyectopasubir.java
@@ -15,7 +15,8 @@ public class Proyectopasubir {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      System.out.println("HOLA ESTE ES MI PROYECTO EN EL REPOSITORIO");
+        System.out.println("primer modificacion");
     }
     
 }
